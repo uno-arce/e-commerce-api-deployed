6fd85aad7ec831e692f2c3bcbd97ed8f13084233
juanmiguel.arce <juanmiguel.arce@cvsu.edu.ph>
2024-01-30 22:21:50 +0800

    Refactor login and register

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ router.post("/register", userControllers.checkUserExists, userControllers.regist
 
 router.post("/login", userControllers.login);
 
-router.get("/details", verify, userControllers.getDetails);
+router.post("/details", verify, userControllers.getDetails);
 
 router.get("/:userId/user", userControllers.getProfile);
 
